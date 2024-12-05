d218b44fa7b0132c0378be8fa68529d3ef7e0e55
donutboyy <dndanielng929@gmail.com>
2024-12-05 12:50:32 +0800

    feat: add initial db revision and alembic

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 
 
 class Event(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int = Field(default=0, primary_key=True)
     name: str = Field(index=True)
     datetime: int = Field(default=0)
-    duration: int | None = Field(default=None)
+    duration: int = Field(default=0)
