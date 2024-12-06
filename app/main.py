d86d9352871ae6d0be9597f5e9d08a7fb8dc26a6
donutboyy <dndanielng929@gmail.com>
2024-12-06 15:45:37 +0800

    feat: login endpoint and new user table

--- app/main.py
+++ app/main.py
@@ -1,12 +1,118 @@
 from typing import Annotated
+from datetime import datetime, timedelta, timezone
 
-from fastapi import FastAPI, Query
+import jwt
+from fastapi import Depends, FastAPI, Query, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodel import select
-from app.models import Event
+from jwt.exceptions import InvalidTokenError
+from app.models import Event, User, UserInDB, Token, TokenData
 from app.db import SessionDep
+from passlib.context import CryptContext
+from fastapi.middleware.cors import CORSMiddleware
 
+SECRET_KEY = "07e177b864b8a5685da5017319e57c6744605d7804a98cc068ab4c2ddb2afa14"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def get_user(session: SessionDep, email):
+    user = session.exec(select(UserInDB).where(UserInDB.email == email)).first()
+    if user:
+        return user
+
+
+def authenticate_user(session: SessionDep, email: str, password: str):
+    user = get_user(session, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(session: SessionDep, token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(session, email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep,
+) -> Token:
+    user = authenticate_user(session, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    return current_user
 
 
 @app.post("/event/")
--- app/models.py
+++ app/models.py
@@ -6,3 +6,17 @@ class Event(SQLModel, table=True):
     name: str = Field(index=True)
     datetime: int = Field(default=0)
     duration: int = Field(default=0)
+
+class User(SQLModel):
+    id: int= Field(primary_key=True, index=True)
+    email: str = Field(unique=True)
+
+class UserInDB(User, table=True):
+    hashed_password: str = Field()
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+class TokenData(SQLModel):
+    email: str | None = None
